a123b68a3a618ddde42e1764b4a0b44a72e9b7a4
pandey2029 <107745010+pandey2029@users.noreply.github.com>
2024-08-17 16:46:49 +0530

    Update Branch_Allotment.js

--- server/Branch_Allotment.js
+++ server/Branch_Allotment.js
@@ -1,8 +1,8 @@
 let studentsCache = null; // Global variable to store the student array
 const mongoose = require('mongoose');
 
-const EduDetails = require('./model/EduDetails');
-const PersonalDetails = require('./model/PersonalDetails');
+const EduDetails = require('./models/EduDetails');
+const PersonalDetails = require('./models/PersonalDetails');
 
 async function fetchStudentData() {
     if (!studentsCache) {
